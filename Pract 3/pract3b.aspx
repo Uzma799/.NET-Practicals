7f9442dd4e0561511ebdb1252bc2f818858ea5cc
Uzma Shaikh <129847795+Uzma799@users.noreply.github.com>
2025-02-19 16:11:18 +0530

    Add files via upload

---
+++ Pract 3/pract3b.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="pract3b.aspx.cs" Inherits="Practical_3.pract3b" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Calendar</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+            <asp:Calendar ID="Calendar1" runat="server" BackColor="White" BorderColor="#3366CC" Font-Names="Verdana" Font-Size="8pt" ForeColor="#003399" Height="200px" OnDayRender="Calendar1_DayRender" Width="220px" 
+                OnSelectionChanged="Calendar1_SelectionChanged" BorderWidth="1px" CellPadding="1" DayNameFormat="Shortest" > <DayHeaderStyle Height="1px" ForeColor="#336666" BackColor="#99CCCC" />
+                <NextPrevStyle BorderStyle="Solid" BorderWidth="2px" Font-Size="8pt" ForeColor="#CCCCFF" /> 
+                <OtherMonthDayStyle BorderStyle="Solid" ForeColor="#999999" /> 
+                <SelectedDayStyle BackColor="#009999" ForeColor="#CCFF99" Font-Bold="True" />  
+                <SelectorStyle BackColor="#99CCCC" ForeColor="#336666" />
+                <TitleStyle BackColor="#003399" Font-Bold="True" Font-Size="10pt" ForeColor="#CCCCFF" Height="25px" BorderColor="#3366CC" BorderWidth="1px" />
+                <TodayDayStyle BackColor="#99CCCC" ForeColor="White" /> <WeekendDayStyle Height="50px" BackColor="#CCCCFF" /> </asp:Calendar>
+            <br /> <div>
+            Your Selected Date: <asp:Label ID="Label1" runat="server"></asp:Label>
+            <br />
+            Todays Date:
+            <asp:Label ID="Label2" runat="server"></asp:Label>
+            <br />
+            Ganati vacation start date: <asp:Label ID="Label3" runat="server"></asp:Label>
+            <br />
+            Days Remainig for Ganpati Vacation:
+            <asp:Label ID="Label4" runat="server"></asp:Label>
+            <br />
+            Days Remaining for Christmas:
+            <asp:Label ID="Label5" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Result" />
+&nbsp;<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Reset" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Pract 3/pract3b.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical_3
+{
+    public partial class pract3b : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Calendar1.Caption = "Uzma Shaikh";
+            Calendar1.FirstDayOfWeek = FirstDayOfWeek.Sunday;
+            Calendar1.NextPrevFormat = NextPrevFormat.ShortMonth;
+            Calendar1.TitleFormat = TitleFormat.Month;
+            Label1.Text = Calendar1.SelectedDate.Date.ToString();
+            Label2.Text = Calendar1.TodaysDate.ToShortDateString();
+            Label3.Text = "10-24-2022";
+            TimeSpan d1 = new DateTime(2022, 10, 24) - DateTime.Now;
+            Label4.Text = d1.Days.ToString();
+            TimeSpan d2 = new DateTime(2022, 12, 25) - DateTime.Now;
+            Label5.Text = d2.Days.ToString();
+            if (Calendar1.SelectedDate.ToShortDateString() == "10-24-2022")
+                Label3.Text = "<b>Ganpati Festival Start</b>";
+            if (Calendar1.SelectedDate.ToShortDateString() == "4-18-2022")
+                Label3.Text = "<b>Ganpati Festival End</b>";
+        }
+
+        protected void Calendar1_DayRender(object sender, System.Web.UI.WebControls.DayRenderEventArgs e)
+        {
+            if (e.Day.Date.Day == 31 && e.Day.Date.Month == 8)
+            {
+                Label lb2 = new Label();
+                lb2.Text = "<br>Ganpati";
+                e.Cell.Controls.Add(lb2);
+                Image g2 = new Image();
+                g2.ImageUrl = "td.jpg";
+                g2.Height = 20;
+                g2.Width = 20;
+                e.Cell.Controls.Add(g2);
+            }
+            if (e.Day.Date.Day == 5 && e.Day.Date.Month == 9)
+            {
+                e.Cell.BackColor = System.Drawing.Color.Yellow;
+                Label lb1 = new Label();
+                lb1.Text = "<br>Teachers day!";
+                e.Cell.Controls.Add(lb1);
+                Image g1 = new Image();
+                g1.ImageUrl = "td.jpg";
+                g1.Height = 20;
+                g1.Width = 20;
+                e.Cell.Controls.Add(g1);
+            }
+        }
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label1.Text = "";
+            Label2.Text = "";
+            Label3.Text = "";
+            Label4.Text = "";
+            Label5.Text = "";
+            Calendar1.SelectedDates.Clear();
+        }
+    }
+}
