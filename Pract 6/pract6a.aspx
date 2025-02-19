de5813a45d56f282d5d8b8eca1437e9a8c3bfc25
Uzma Shaikh <129847795+Uzma799@users.noreply.github.com>
2025-02-19 16:13:21 +0530

    Add files via upload

---
+++ Pract 6/pract6a.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="pract6a.aspx.cs" Inherits="Practical_6.pract6a" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+&nbsp;<br />
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server" TextMode="MultiLine"></asp:TextBox>
+            <br />
+            <br />
+            <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:ConnectionString %>" SelectCommand="SELECT * FROM [Emp]"></asp:SqlDataSource>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Pract 6/pract6a.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Practical_6
+{
+    public partial class pract6a : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database.mdf;Integrated Security=True");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader ds;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            cmd.Connection = con;
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+           cmd.CommandText = "select*from Table where name='" + TextBox1.Text + "';";
+            ds = cmd.ExecuteReader();
+            while(ds.Read())
+            {
+                TextBox2.Text += ds[0].ToString() + "\t" + ds[1].ToString() + "\t" + ds[2].ToString() + "\n";
+            }
+
+        }
+    }
+}
