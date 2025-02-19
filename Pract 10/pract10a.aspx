8bc164bf0d5e6992f5ff11caeea882298c61b513
Uzma Shaikh <129847795+Uzma799@users.noreply.github.com>
2025-02-19 16:14:18 +0530

    Add files via upload

---
+++ Pract 10/pract10a.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="pract10a.aspx.cs" Inherits="practical_10.pract10a" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="xml writer" />
+            <br />
+            <br />
+            <asp:ListBox ID="ListBox1" runat="server" Height="163px" Width="212px"></asp:ListBox>
+            <br />
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="xml reader" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Pract 10/pract10a.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml;
+
+namespace practical_10
+{
+    public partial class pract10a : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            XmlTextWriter writer = new XmlTextWriter("C: \Users\\samir\\source\repos\\practical 10\\practical 10\\XMLFile.xml", null);
+
+            writer.WriteStartDocument();
+            writer.WriteStartElement("details");
+            writer.WriteElementString("Id", "1");
+            writer.WriteElementString("name", "Uzma");
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Close();
+            Label1.Text = "data write sucessfully";
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            String xmlNode = "C: \\Users\\samir\\source\repos\\practical 10\\practical 10\\XMLFile.xml";
+            XmlReader xreader = XmlReader.Create(xmlNode);
+            while (xreader.Read())
+            {
+                switch (xreader.NodeType)
+                {
+                    case
+                    XmlNodeType.Element: ListBox1.Items.Add("<" + xreader.Name + ">");
+                        break;
+                    case XmlNodeType.Text:
+                        ListBox1.Items.Add(xreader.Value);
+                        break;
+                    case XmlNodeType.EndElement:
+                        ListBox1.Items.Add("<" + xreader.Name + ">");
+                        break;
+                }
+            }
+        }
+    }
+}
